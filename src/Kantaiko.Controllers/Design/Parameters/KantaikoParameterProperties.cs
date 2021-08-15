7ca4b86bf920dfa6a81c7e5de6acf31d64fb20de
Exeteres <exeteres@yandex.ru>
2021-08-15 11:37:42 +0700

    feat: Add IsHidden parameter design property

--- src/Kantaiko.Controllers/Design/Parameters/KantaikoParameterProperties.cs
+++ src/Kantaiko.Controllers/Design/Parameters/KantaikoParameterProperties.cs
@@ -6,5 +6,6 @@
 
         public const string Name = Prefix + nameof(Name);
         public const string IsOptional = Prefix + nameof(IsOptional);
+        public const string IsHidden = Prefix + nameof(IsHidden);
     }
 }
--- src/Kantaiko.Controllers/FromServicesAttribute.cs
+++ src/Kantaiko.Controllers/FromServicesAttribute.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using Kantaiko.Controllers.Converters;
 using Kantaiko.Controllers.Design.Parameters;
+using Kantaiko.Controllers.Design.Properties;
 
 namespace Kantaiko.Controllers
 {
     [AttributeUsage(AttributeTargets.Parameter)]
-    public class FromServicesAttribute : Attribute, IParameterConverterTypeProvider
+    public class FromServicesAttribute : Attribute, IParameterConverterTypeProvider, IParameterDesignPropertyProvider
     {
         public Type GetConverterType(EndpointParameterDesignContext context) => typeof(ServiceConverter);
+
+        public DesignPropertyCollection GetParameterDesignProperties() => new()
+        {
+            [KantaikoParameterProperties.IsHidden] = true
+        };
     }
 }
