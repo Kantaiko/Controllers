c8af6b818fdabe5f6cc5310b59ef5abf8428d2df
Exeteres <exeteres@yandex.ru>
2022-02-05 12:39:23 +0700

    fix: Use all available property providers in PropertyProviderIntrospectionInfoTransformer

--- src/Kantaiko.Controllers/Introspection/Factory/Transformers/PropertyProviderIntrospectionInfoTransformer.cs
+++ src/Kantaiko.Controllers/Introspection/Factory/Transformers/PropertyProviderIntrospectionInfoTransformer.cs
@@ -1,54 +1,71 @@
 ﻿using System.Linq;
 using Kantaiko.Controllers.Introspection.Factory.Attributes;
 using Kantaiko.Controllers.Introspection.Factory.Context;
-using Kantaiko.Properties.Immutable;
 
 namespace Kantaiko.Controllers.Introspection.Factory.Transformers;
 
 public class PropertyProviderIntrospectionInfoTransformer : IntrospectionInfoTransformer
 {
-    protected override IImmutablePropertyCollection TransformControllerProperties(ControllerFactoryContext context)
+    protected override ControllerInfo TransformController(ControllerFactoryContext context)
     {
         var propertyProviders = context.Controller.Type.GetCustomAttributes(true)
             .OfType<IControllerPropertyProvider>();
 
-        var properties = context.Controller.Properties;
-
         foreach (var propertyProvider in propertyProviders)
         {
-            properties = propertyProvider.UpdateControllerProperties(context);
+            var properties = propertyProvider.UpdateControllerProperties(context);
+
+            context = new ControllerFactoryContext(
+                context.Controller with { Properties = properties },
+                context.ServiceProvider
+            );
         }
 
-        return properties;
+        return context.Controller with
+        {
+            Endpoints = TransformEndpoints(context)
+        };
     }
 
-    protected override IImmutablePropertyCollection TransformEndpointProperties(EndpointFactoryContext context)
+    protected override EndpointInfo TransformEndpoint(EndpointFactoryContext context)
     {
         var propertyProviders = context.Endpoint.MethodInfo.GetCustomAttributes(true)
             .OfType<IEndpointPropertyProvider>();
 
-        var properties = context.Endpoint.Properties;
-
         foreach (var propertyProvider in propertyProviders)
         {
-            properties = propertyProvider.UpdateEndpointProperties(context);
+            var properties = propertyProvider.UpdateEndpointProperties(context);
+
+            context = new EndpointFactoryContext(
+                context.Endpoint with { Properties = properties },
+                context.ServiceProvider
+            );
         }
 
-        return properties;
+        return context.Endpoint with
+        {
+            ParameterTree = TransformParameterTree(context)
+        };
     }
 
-    protected override IImmutablePropertyCollection TransformParameterProperties(ParameterFactoryContext context)
+    protected override EndpointParameterInfo TransformParameter(ParameterFactoryContext context)
     {
         var propertyProviders = context.Parameter.AttributeProvider.GetCustomAttributes(true)
             .OfType<IParameterPropertyProvider>();
 
-        var properties = context.Parameter.Properties;
-
         foreach (var propertyProvider in propertyProviders)
         {
-            properties = propertyProvider.UpdateParameterProperties(context);
+            var properties = propertyProvider.UpdateParameterProperties(context);
+
+            context = new ParameterFactoryContext(
+                context.Parameter with { Properties = properties },
+                context.ServiceProvider
+            );
         }
 
-        return properties;
+        return context.Parameter with
+        {
+            Children = TransformParameters(context.Parameter.Children, context.ServiceProvider)
+        };
     }
 }
